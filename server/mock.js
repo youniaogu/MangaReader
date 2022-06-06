01cdb67f192a0b42fa1c052c989076b626cb4f15
youniaogu <luo1257857309@gmail.com>
2022-06-06 15:17:24 +0800

    better mock function

--- server/mock.js
+++ server/mock.js
@@ -13,39 +13,12 @@ function logFail(message) {
   console.log(chalk.red(message));
 }
 
-app.all('/update', function (req, res) {
-  res.sendFile(path.join(__dirname, 'example/MHG/update.html'), (err) => {
+app.all('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'example', req.path), (err) => {
     if (err) {
-      logFail('【fail】update: ' + err.message);
+      logFail('【fail】' + req.path);
     } else {
-      logSuccess('【success】update');
-    }
-  });
-});
-app.all('/search', function (req, res) {
-  res.sendFile(path.join(__dirname, 'example/MHG/search.html'), (err) => {
-    if (err) {
-      logFail('【fail】search: ' + err.message);
-    } else {
-      logSuccess('【success】search');
-    }
-  });
-});
-app.all('/manga', function (req, res) {
-  res.sendFile(path.join(__dirname, 'example/MHG/manga.html'), (err) => {
-    if (err) {
-      logFail('【fail】manga: ' + err.message);
-    } else {
-      logSuccess('【success】manga');
-    }
-  });
-});
-app.all('/chapter', function (req, res) {
-  res.sendFile(path.join(__dirname, 'example/MHG/chapter.html'), (err) => {
-    if (err) {
-      logFail('【fail】chapter: ' + err.message);
-    } else {
-      logSuccess('【success】chapter');
+      logSuccess('【success】' + req.path);
     }
   });
 });
