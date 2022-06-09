80220aefdd9cf645994743884241dc00a6a72c5f
youniaogu <luo1257857309@gmail.com>
2022-06-09 10:52:07 +0800

    remove http cache of image

--- src/plugins/mhg.ts
+++ src/plugins/mhg.ts
@@ -342,6 +342,7 @@ class ManHuaGui extends Base {
             'sec-fetch-dest': 'image',
             'sec-fetch-mode': 'no-cors',
             'sec-fetch-site': 'cross-site',
+            'Cache-control': 'no-cache',
             'user-agent':
               'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
           },
--- src/plugins/mhgm.ts
+++ src/plugins/mhgm.ts
@@ -323,6 +323,7 @@ class ManHuaGuiMobile extends Base {
             'sec-fetch-dest': 'image',
             'sec-fetch-mode': 'no-cors',
             'sec-fetch-site': 'cross-site',
+            'Cache-control': 'no-cache',
             'user-agent':
               'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
           },
