ee18ad79662c464464f45a13871c38ad431fbf7d
youniaogu <luo1257857309@gmail.com>
2023-02-14 18:28:03 +0800

    update precommit.js

--- precommit.js
+++ precommit.js
@@ -1,18 +1,25 @@
 #!/usr/bin/env node
-const path = require('path')
-const fs = require('fs')
+const path = require('path');
+const fs = require('fs');
 
 const DEFAULT_BUNDLE_IDENTIFIER = 'personal.bundle.identifier.MangaReader';
 const PATTERN_PRODUCT_BUNDLE_IDENTIFIER = /PRODUCT_BUNDLE_IDENTIFIER = (.+);/g;
 
-const fileContent = fs
-  .readFileSync(path.resolve('ios/MangaReader.xcodeproj/project.pbxproj'))
-  .toString();
+const revision = require('child_process')
+  .execSync('git diff --cached --name-only --diff-filter=ACM')
+  .toString()
+  .trim();
 
-[...fileContent.matchAll(PATTERN_PRODUCT_BUNDLE_IDENTIFIER)].forEach((item) => {
-  const [, bundleIdentifier] = item || [];
+if (revision.includes('ios/MangaReader.xcodeproj/project.pbxproj')) {
+  const fileContent = fs
+    .readFileSync(path.resolve('ios/MangaReader.xcodeproj/project.pbxproj'))
+    .toString();
 
-  if (bundleIdentifier !== DEFAULT_BUNDLE_IDENTIFIER) {
-    throw new Error('开源项目请不要上传私人bundle identifier');
-  }
-});
+  [...fileContent.matchAll(PATTERN_PRODUCT_BUNDLE_IDENTIFIER)].forEach((item) => {
+    const [, bundleIdentifier] = item || [];
+
+    if (bundleIdentifier !== DEFAULT_BUNDLE_IDENTIFIER) {
+      throw new Error('开源项目请不要上传私人bundle identifier');
+    }
+  });
+}
