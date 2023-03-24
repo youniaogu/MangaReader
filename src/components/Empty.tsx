3f5fd439433196f5349e87e9d594ae283d44058b
youniaogu <luo1257857309@gmail.com>
2023-03-24 15:38:45 +0800

    don't push

--- src/components/Empty.tsx
+++ src/components/Empty.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Center, Heading, Image } from 'native-base';
+import { Center, Heading, Image, Pressable } from 'native-base';
 
 const taptapGif = require('~/assets/tap_tap.gif');
 
 interface EmptyProps {
   text?: string;
+  onPress?: () => void;
 }
 
-const Empty = ({ text = 'Nothing Here' }: EmptyProps) => {
+const Empty = ({ text = 'Nothing Here', onPress }: EmptyProps) => {
   return (
     <Center w="full" h="full" pb={32}>
-      <Image w={32} h={48} resizeMode="contain" source={taptapGif} alt="taptap" />
-      <Heading color="gray.500" fontWeight="bold" fontSize="md">
-        {text}
-      </Heading>
+      <Pressable onPress={onPress}>
+        <Image w={32} h={48} resizeMode="contain" source={taptapGif} alt="taptap" />
+        <Heading color="gray.500" fontWeight="bold" fontSize="md">
+          {text}
+        </Heading>
+      </Pressable>
     </Center>
   );
 };
