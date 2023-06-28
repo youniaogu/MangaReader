09c90d5c7dfcc2fea8022ac8d9cddd8cbcc774d1
youniaogu <youniaogu2023@gmail.com>
2023-06-28 09:36:16 +0800

    setup module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '^~/(.+)': './src/\\1',
+        },
+        extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.json', '.tsx', '.ts', '.native.js'],
+      },
+    ],
+    ['react-native-reanimated/plugin'],
+  ],
 };
