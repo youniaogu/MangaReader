92ded19dc3e1e6439ff9e6bee8a212298ad0b79f
rxw22 <2587897409@qq.com>
2024-03-31 14:05:53 +0800

    fix: 条漫模式下反向滚动画面抖动

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,21 +1,32 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FileSystem, Dirs } from 'react-native-file-access';
 import { ImageState } from '~/components/ComicImage';
 
 type CacheMap = Record<string, ImageState>;
 
 class Cache {
-  private _comicCacheKey: string;
+  private _basePath = `${Dirs.DocumentDir}/@cache`; // 或者Dirs.CacheDir？
+  private _path: string;
   private _cacheMap: CacheMap = {};
 
   constructor(identification: string) {
-    this._comicCacheKey = `CACHE_${identification}`;
+    this._path = `${this._basePath}/${identification}.json`;
   }
 
   async initCacheMap() {
-    const value = await AsyncStorage.getItem(this._comicCacheKey);
-    console.log('readData: ', JSON.stringify(value));
-    if (value != null) {
-      this._cacheMap = JSON.parse(value);
+    try {
+      const dirExists = await FileSystem.exists(this._basePath);
+      if (!dirExists) {
+        await FileSystem.mkdir(this._basePath);
+      }
+      const exists = await FileSystem.exists(this._path);
+      if (exists) {
+        const content = await FileSystem.readFile(this._path);
+        this._cacheMap = JSON.parse(content);
+      } else {
+        await FileSystem.writeFile(this._path, JSON.stringify({}));
+      }
+    } catch (error) {
+      console.error('An error in initCacheMap:', error);
     }
   }
 
@@ -29,16 +40,22 @@ class Cache {
 
   async storeCacheMap() {
     try {
-      const keys = await AsyncStorage.getAllKeys();
-      const cacheKeys = keys.filter((key) => key.includes('CACHE_'));
-      // 限制只存50条
-      if (cacheKeys.length >= 50) {
-        await AsyncStorage.removeItem(cacheKeys[0]);
-      }
-      console.log('_cacheMap: ', JSON.stringify(this._cacheMap));
-      await AsyncStorage.setItem(this._comicCacheKey, JSON.stringify(this._cacheMap));
+      await FileSystem.writeFile(this._path, JSON.stringify(this._cacheMap));
     } catch (error) {
-      console.log('setCache error', error);
+      console.error('An error in storeCacheMap:', error);
+    }
+  }
+
+  // 清除缓存
+  static async clearCache() {
+    const basePath = `${Dirs.DocumentDir}/@cache`;
+    try {
+      const dirExists = await FileSystem.exists(basePath);
+      if (dirExists) {
+        await FileSystem.unlink(basePath);
+      }
+    } catch (error) {
+      console.error('An error in clearCache:', error);
     }
   }
 }
