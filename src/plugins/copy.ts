f03c0639e44a24ee779d60757a592ada47d35e9e
raoxiongwen <raoxiongwen@eelink.com.cn>
2025-06-06 09:32:40 +0800

    修改拷贝插件搜索接口地址

--- src/plugins/copy.ts
+++ src/plugins/copy.ts
@@ -211,8 +211,8 @@ class CopyManga extends Base {
   };
   prepareSearchFetch: Base['prepareSearchFetch'] = (keyword, page) => {
     return {
-      // url: 'https://api.mangacopy.com/api/v3/search/comic',
-      url: 'https://api.mangacopy.com/api/kb/web/searchbd/comics', // web api
+      // url: 'https://api.mangacopy.com/api/v3/search/comic', // 可能换地址了
+      url: 'https://api.mangacopy.com/api/kb/web/searchc/comics', // web api
       body: {
         // platform: 1,
         q: keyword,
