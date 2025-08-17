22843c97b8a0a90e1ca5c3af72d2677be7322e83
TypeDreamMoon <1067823908@qq.com>
2025-08-17 22:10:43 +0800

    Fixed the issue of line breaks being reported as errors

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  printWidth: 100,
-  singleQuote: true,
-  trailingComma: 'es5',
+  printWidth: 100,       // 一行最大长度
+  singleQuote: true,     // 使用单引号
+  trailingComma: 'es5',  // 在 ES5 中合法的地方加尾随逗号（对象、数组等）
+  semi: true,            // 语句末尾加分号
+  tabWidth: 2,           // 缩进两个空格
+  endOfLine: 'auto'      // 自动识别换行符 (解决 Windows/Mac/Linux 差异)
 };
